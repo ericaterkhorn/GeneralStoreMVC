2a76df62c55dab0ea3769d6cb38f98b8ecb7d1d1
ericaterkhorn <erica.terkhorn@gmail.com>
2020-12-04 15:32:53 -0500

    note delete

---
+++ GeneralStore.MVC/Controllers/TransactionController.cs
@@ -0,0 +1,85 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: Transaction
+        public ActionResult Index()
+        {
+            return View(_db.Transactions.ToArray());
+        }
+        //Viewdata/viewbags
+        //GET: Transaction/Create
+        public ActionResult Create()
+        {
+            var viewModel = new CreateTransactionViewModel();
+            viewModel.Products = _db.Products.Select(p => new SelectListItem
+            {
+                Text = p.Name,
+                Value = p.ProductID.ToString()
+            });
+            viewModel.Customers = _db.Customers.Select(p => new SelectListItem
+            {
+                Text = p.FullName,
+                Value = p.CustomerID.ToString()
+            });
+            return View(viewModel);
+        }
+
+        //POST: Transaction/create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CreateTransactionViewModel viewModel)
+        {
+            _db.Transactions.Add(new Transaction
+            {
+                CustomerID = viewModel.CustomerID,
+                ProductID = viewModel.ProductID
+            });
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //GET: Transaction/delete/id
+        [Route("delete/{id}")]
+        public ActionResult Delete([Required] int id)
+        {
+            Transaction transaction = _db.Transactions.Find(id);
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(transaction);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Transaction model)
+        {
+            var transaction = _db.Transactions.Find(model.TransactionID);
+            _db.Transactions.Remove(transaction);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [Route("edit/{id}")]
+        public ActionResult Edit(int id)
+        {
+            Transaction transaction = _db.Transactions.Find(id);
+            if (transaction == null)
+            {
+                return View("NotFound");
+            }
+            ViewData["Customers"]
+        }
+    }
+}
---
+++ GeneralStore.MVC/Models/CreateTransactionViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Models
+{
+    public class CreateTransactionViewModel
+    {
+        //Drop down products
+        public IEnumerable<SelectListItem> Products { get; set; }
+        //Selected product
+        public int ProductID { get; set; }
+        //Drop down customers
+        public IEnumerable<SelectListItem> Customers { get; set; }
+        //Selected customer
+        public int CustomerID { get; set; }
+    }
+}
